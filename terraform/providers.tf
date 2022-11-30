4f70263ff8015e56fdb41c2d4e21a1402f9a2a5b
fabio-fcr <106354759+fabio-fcr@users.noreply.github.com>
2022-11-29 23:51:26 -0300

    add vpc no arquivo main.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.43.0"
     }
   }
 }
 
 provider "aws" {
-  # Configuration options
+  region = "us-east-1"
 }

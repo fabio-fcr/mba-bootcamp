25e7b0a92927deb1c93ee175d25975cae81110fe
fabio-fcr <106354759+fabio-fcr@users.noreply.github.com>
2022-11-30 23:52:55 -0300

    criado recursos de rede na AWS subnet, IGW e RTs

---
+++ terraform/network.tf
@@ -0,0 +1,54 @@
+resource "aws_vpc" "bootcamp" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "bootCamp"
+  }
+}
+
+resource "aws_subnet" "bootcamp-private-subnet" {
+  vpc_id            = aws_vpc.bootcamp.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "bootcamp-private-subnet"
+  }
+}
+
+resource "aws_subnet" "bootcamp-public-subnet" {
+  vpc_id            = aws_vpc.bootcamp.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "bootcamp-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "bootcamp-igw" {
+  vpc_id = aws_vpc.bootcamp.id
+
+  tags = {
+    Name = "bootcamp-iwg"
+  }
+}
+
+resource "aws_internet_gateway_attachment" "bootcamp-igw-attach" {
+  count               = 0
+  internet_gateway_id = aws_internet_gateway.bootcamp-igw.id
+  vpc_id              = aws_vpc.bootcamp.id
+}
+
+resource "aws_route_table" "bootcamp-public-rt" {
+  vpc_id = aws_vpc.bootcamp.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.bootcamp-igw.id
+  }
+
+  tags = {
+    Name = "bootcamp-public-rt"
+  }
+}
